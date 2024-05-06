88c50d7581be28613152ff14914bf154a17ba1ea
TOBIAS IAN JAJURIN <48675805@ort>
2024-05-06 08:43:17 -0300

    Creacion de la clase cliente

---
+++ cliente.cs
@@ -0,0 +1,11 @@
+class cliente 
+{
+    public int DNI {get; private set;}
+    public string APELLIDO { get; private set; }   
+    public string NOMBRE  { get; private set; }
+    public DateTime FECHAINSCRIPCCION { get; set; }
+    public int TIPOENTRADA { get; set; }
+    public int CANTIDAD {get; set;}
+
+
+}
